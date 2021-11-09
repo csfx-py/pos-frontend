499e2049ea15ef157a7799e123355074f84aa887
csfx-py <csfxshreyas@gmail.com>
2021-11-09 10:20:10 +0530

    fixed filter object keys

--- src/Components/FilterSelect.js
+++ src/Components/FilterSelect.js
@@ -21,24 +21,24 @@ function FilterSelect({ value, handleFilter, data, name }) {
 
   return (
     <Grid item>
-        <InputLabel>{name}</InputLabel>
-        <Select
-          name={name}
-          value={value}
-          onChange={handleFilter}
-          className={classes.select}
-          MenuProps={{ classes: { paper: classes.menuPaper } }}
-        >
-          <MenuItem value="">
-            <em>All</em>
-          </MenuItem>
-          {data.length > 0 &&
-            data.map((datum, index) => (
-              <MenuItem key={index} value={datum}>
-                {datum}
-              </MenuItem>
-            ))}
-        </Select>
+      <InputLabel>{name}</InputLabel>
+      <Select
+        name={name}
+        value={value}
+        onChange={handleFilter}
+        className={classes.select}
+        MenuProps={{ classes: { paper: classes.menuPaper } }}
+      >
+        <MenuItem value="">
+          <em>All</em>
+        </MenuItem>
+        {data.length > 0 &&
+          data.map((datum, index) => (
+            <MenuItem key={index} value={datum.name || datum.size}>
+              {datum.name || datum.size}
+            </MenuItem>
+          ))}
+      </Select>
     </Grid>
   );
 }
