7ca14ce35562cb6a14a02236982c33e82c439842
csfx-py <csfxshreyas@gmail.com>
2022-01-25 17:06:37 +0530

    added purchase report printing

--- src/views/Shop/Reports/PurchaseReport.js
+++ src/views/Shop/Reports/PurchaseReport.js
@@ -3,19 +3,13 @@ import { useContext, useState } from "react";
 import DataTable from "../../../Components/DataTable";
 import { ShopDataContext } from "../../../Contexts/ShopDataContext";
 import { UtilityContext } from "../../../Contexts/UtilityContext";
+import printPurchase from "../../../utils/printPurchase";
 
 const columns = [
   {
     id: "name",
     label: "Name",
     minWidth: 200,
-  },
-  {
-    id: "price",
-    label: "Price",
-    minWidth: 50,
-    align: "right",
-    format: (value) => value.toLocaleString("en-IN"),
   },
   {
     id: "qty_case",
@@ -28,6 +22,27 @@ const columns = [
     id: "qty_item",
     label: "Item Qty",
     minWidth: 50,
+    align: "right",
+    format: (value) => value.toLocaleString("en-IN"),
+  },
+  {
+    id: "total",
+    label: "Qty",
+    minWidth: 100,
+    align: "right",
+    format: (value) => value.toLocaleString("en-IN"),
+  },
+  {
+    id: "price",
+    label: "Price",
+    minWidth: 50,
+    align: "right",
+    format: (value) => value.toLocaleString("en-IN"),
+  },
+  {
+    id: "amt",
+    label: "Amt",
+    minWidth: 100,
     align: "right",
     format: (value) => value.toLocaleString("en-IN"),
   },
@@ -79,10 +94,28 @@ function PurchaseReports() {
             Generate Report
           </Button>
         </Grid>
+        <Grid item xs={2}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={async (e) => {
+              if (rows.length > 0) {
+                printPurchase(rows);
+              }
+            }}
+          >
+            Print Report
+          </Button>
+        </Grid>
       </Grid>
       <Grid item xs={12}>
         <DataTable rows={rows} columns={columns} />
       </Grid>
+      <iframe
+        title="invoice"
+        id="dsr-print"
+        style={{ height: "0px", width: "0px", position: "absolute" }}
+      ></iframe>
     </Grid>
   );
 }
